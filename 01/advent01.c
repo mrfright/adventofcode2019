6e6e6e40e77870fda9572e7aa13cade17f9a0454
Eric Faehnrich <Eric_Faehnrich@LincolnElectric.com>
2019-12-05 13:13:38 -0500

    start main

--- 01/advent01.c
+++ 01/advent01.c
@@ -1,2 +1,16 @@
 #include <stdio.h>
-int main(){ printf("Hello, world.\n"); }
+int main(){
+    FILE *fp = fopen("testfile", "r");
+    if(fp == NULL) {
+        printf("couldn't open testfile\n");
+        exit(EXIT_FAILURE);
+    }
+
+    ssize_t read;
+    char *line = NULL;
+    size_t len = 0;
+    while((read = getline(&line, &len, fp)) != -1) {
+        
+    }    
+}
+        
